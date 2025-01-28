fa245c4b70c094ab77aa8b8aecbe9722c23413e7
Rohit <dv6510@gmail.com>
2025-01-28 21:49:15 +0530

    made some updates

--- code-engine-images/cpp/cpp-sandbox.sh
+++ code-engine-images/cpp/cpp-sandbox.sh
@@ -1,1 +1,9 @@
-COPY cpp-sandbox.sh /cpp-sandbox.sh
+#!/bin/sh
+# Limit execution to 5 seconds
+timeout -k 2 5 \
+    seccomp-proxy \
+        --syscalls @safe \
+        --execve \
+        -- \
+        g++ -x c++ -static -O2 -Wall -Wextra -Werror -o /tmp/a.out - && \
+        /tmp/a.out
