147b8cf37d59af5d10a7ceb0698c0b6d868ca7c9
Rohit <dv6510@gmail.com>
2025-01-28 21:51:43 +0530

    Stop tracking code-engine-images/cpp directory

--- code-engine-images/cpp/cpp-sandbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Limit execution to 5 seconds
-timeout -k 2 5 \
-    seccomp-proxy \
-        --syscalls @safe \
-        --execve \
-        -- \
-        g++ -x c++ -static -O2 -Wall -Wextra -Werror -o /tmp/a.out - && \
-        /tmp/a.out
